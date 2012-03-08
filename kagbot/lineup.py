ec93a50df3cff8c478aac954c5c2aa22ae254b2b
Denis Usanov <usanovdd@gmail.com>
2012-03-08 14:14:18 +0400

    Added part of interface of lineup

---
+++ kagbot/lineup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class LineUp:
+	def __init__ (self):
+		pass
+	
+	def add (self, player):
+		pass
+	
+	def delete (self, player):
+		pass
+	
+	def shuffle (self, isBalance=False):
+		pass
+
+
+if __name__ == "__main__":
+    la = LineUp()
+    la.add("Hi")
+    la.shuffle()
